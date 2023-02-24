f65ede0b48744a40cb2e6421a5069f50d4d104be
Chanho Kim <cksgh1565@gmail.com>
2023-02-24 14:52:42 +0900

    로그인, 인증, 로그아웃

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const cookieparser = require('cookie-parser');
 const config = require('./config/key');
 
 const { User } = require("./models/User");
+const {auth} = require("./middleware/auth");
 
 
 // application/x-www-form-urlencoded
@@ -16,6 +17,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 // application/json
 app.use(bodyParser.json());
 app.use(cookieparser());
+
 
 const mongoose = require('mongoose')
 mongoose.set('strictQuery', false)
@@ -26,7 +28,7 @@ mongoose.connect(config.mongoURI, {
 
 app.get('/', (req, res) => res.send('Hello World!~~'))
 
-app.post('/register', (req, res) => {
+app.post('/api/users/register', (req, res) => {
 
     // 회원 가입 할때 필요한 정보들을 client에서 가죠오면 그것들을 데이터 베이스에 넣어준다.
     const user = new User(req.body)
@@ -41,7 +43,7 @@ app.post('/register', (req, res) => {
 
 })
 
-app.post('/login', (req, res) => {
+app.post('/api/users/login', (req, res) => {
     
     // 요청된 이메일을 데이터베이스에 있는지 찾는다
     User.findOne({email: req.body.email}, (err, user) => {
@@ -67,6 +69,31 @@ app.post('/login', (req, res) => {
                 .json({loginSuccess: true, userId: user._id});
 
             })
+        })
+    })
+})
+
+app.get('/api/users/auth', auth, (req, res) => {
+    
+    // 여기까지 미들웨어를 통과해 왔다는 얘기는 Authentication이 True라는 말.
+    res.status(200).json({
+        _id: req.user._id,
+        isAdmin: req.user.role === 0? false : true,
+        isAuth: true,
+        email: req.user.email,
+        name: req.user.name,
+        lastname: req.user.lastname,
+        role: req.user.role,
+        image: req.user.image
+    })
+})
+
+app.get('/api/users/logout', auth, (req, res) => {
+    
+    User.findOneAndUpdate({_id: req.user._id}, {token: ""}, (err, user) => {
+        if(err) return res.json({success: false, err})
+        return res.status(200).send({
+            success: true
         })
     })
 })
